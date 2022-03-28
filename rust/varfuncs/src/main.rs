cdb24c6347d2605abf1658786da71c1735ec1dde
yuange <yuanyeex@gmail.com>
2022-03-28 22:26:10 +0800

    intresting: block is also an expression.

--- rust/varfuncs/src/main.rs
+++ rust/varfuncs/src/main.rs
@@ -7,6 +7,8 @@ fn main() {
     println!("10 + 320 = {}", sumup);
     println!("20 + 320 = {}", sum(20, 320));
 
+    block_express();
+
     println!("Input you name and press <ENTER>:");
     io::stdin().read_line(&mut buf).expect("read line from io failed");
     hello_world(buf);
@@ -18,4 +20,16 @@ fn hello_world(name: String) {
 
 fn sum(x: i32, y: i32) -> i32 {
     return x + y;
+}
+
+fn block_express() {
+    // block is also an express which evaluate to a value
+    let sum= {
+        let a = 100;
+        let b = 90;
+        // cannot return and cannot add ';'
+        a + b
+    };
+
+    println!("sum 100 and 90 in a block: {}", sum);
 }

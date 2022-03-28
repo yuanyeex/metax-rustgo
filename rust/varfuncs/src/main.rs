a2dfeb2cb616facab6a73cf4e1da41a3e3e96f71
yuanyeex <yuanyeex@gmail.com>
2022-03-28 23:00:13 +0800

    guess number using 'if' as flow control

--- rust/varfuncs/src/main.rs
+++ rust/varfuncs/src/main.rs
@@ -1,4 +1,6 @@
 use std::io;
+use rand::Rng;
+
 fn main() {
     println!("Hello, world!");
     let mut buf: String = String::new();
@@ -12,6 +14,10 @@ fn main() {
     println!("Input you name and press <ENTER>:");
     io::stdin().read_line(&mut buf).expect("read line from io failed");
     hello_world(buf);
+
+    // guess number again here, but we use if as the flow control instead of match.
+    let secret = rand::thread_rng().gen_range(1, 100);
+    guess_number(secret);
 }
 
 fn hello_world(name: String) {
@@ -32,4 +38,30 @@ fn block_express() {
     };
 
     println!("sum 100 and 90 in a block: {}", sum);
+}
+
+fn guess_number(secret_number: u32) {
+    println!("The secret is: {}", secret_number);
+    
+    loop {
+        let mut num = String::new();
+        println!("Input your guess number:");
+        io::stdin().read_line(&mut num).expect("read line from io failed");
+        let num:u32 = match  num.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Invalid number input: {} ... ", num.trim());
+                continue;
+            }
+        };
+
+        if num == secret_number {
+            println!("You win!");
+            break;
+        } else if num < secret_number {
+            println!("Too small!");
+        } else {
+            println!("Too larger");
+        }
+    }
 }

ebd4837ffa6ceb202f41ce7498f6c21191432990
yuange <yuanyeex@gmail.com>
2022-04-09 23:21:44 +0800

    Patterns that bind to values

--- rust/enum_match/src/main.rs
+++ rust/enum_match/src/main.rs
@@ -3,7 +3,13 @@ enum Coin {
     Penny,
     Nickel,
     Dime,
-    Quarter,
+    Quarter(UsState),
+}
+#[derive(Debug)]
+enum UsState {
+    Alabama,
+    Alaska,
+    // -- snip -- 
 }
 
 fn value_in_cents(coin: Coin) -> u8 {
@@ -11,7 +17,10 @@ fn value_in_cents(coin: Coin) -> u8 {
         Coin::Penny => 1,
         Coin::Nickel => 5,
         Coin::Dime => 10,
-        Coin::Quarter => 25,
+        Coin::Quarter(state) => {
+            println!("State quater from {:?}", state);
+            25
+        },
     }
 }
 
@@ -19,5 +28,6 @@ fn main() {
     println!("{:?} is {} cents", Coin::Penny, value_in_cents(Coin::Penny));
     println!("{:?} is {} cents", Coin::Nickel, value_in_cents(Coin::Nickel));
     println!("{:?} is {} cents", Coin::Dime, value_in_cents(Coin::Dime));
-    println!("{:?} is {} cents", Coin::Quarter, value_in_cents(Coin::Quarter));
+    println!("{:?} is {} cents", Coin::Quarter(UsState::Alabama), value_in_cents(Coin::Quarter(UsState::Alabama)));
+    println!("{:?} is {} cents", Coin::Quarter(UsState::Alaska), value_in_cents(Coin::Quarter(UsState::Alaska)));
 }

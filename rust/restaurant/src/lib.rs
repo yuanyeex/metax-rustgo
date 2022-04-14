f278f144db314c3050c1653d8a7d474660861fc4
yuanyeex <yuanyeex@gmail.com>
2022-04-14 23:02:02 +0800

    use paths

--- rust/restaurant/src/lib.rs
+++ rust/restaurant/src/lib.rs
@@ -7,6 +7,20 @@ mod tests {
     }
 }
 
+use rand::Rng;
+// nested paths
+use std::{io, cmp::Ordering};
+
+//
+// use std::io;
+// use std::io::Write;
+// ===> 
+// use std::io::{self, Write};
+
+// also, a global operattor imports all public itesm into scop
+// use std::collections::*;
+
+
 /**
  * Paths can take two forms:
  * 1. absolute path: starts from a crate root by using a crate name or a literal crate
@@ -62,6 +76,8 @@ mod back_of_house {
 }
 
 pub fn eat_at_restaurant() {
+    eat_at_restaurant2();
+    hosting::add_to_waitlist();
     // 
     let order1 = back_of_house::Appetizer::Soup;
     let order2 = back_of_house::Appetizer::Salad;
@@ -81,6 +97,11 @@ pub fn eat_at_restaurant() {
     // the next line whon't compile if we uncomment it, we are not allowed
     // to see or modify the seasonal fruit that comes with the meal.
     // meal.seasonal_fruit = String::from("bluebeeries");
+}
 
+// pub 
+use crate::front_of_house::hosting;
 
+pub fn eat_at_restaurant2() {
+    hosting::add_to_waitlist();
 }
